200e6bf3d41929d13790f2fe95fc4c09bcd4cb1e
Daniil <rynkd@mail.ru>
2023-01-25 19:05:08 +0700

    Добавьте файлы проекта.

---
+++ lab12/Controllers/CalcServiceController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab12.Models;
+
+namespace lab12.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Manual()
+        {
+            if (Request.Method == "POST")
+            {
+                try
+                {
+                    var calc = new CalcModel
+                    {
+                        X = Int32.Parse(HttpContext.Request.Form["x"]),
+                        operation = HttpContext.Request.Form["operation"],
+                        Y = Int32.Parse(HttpContext.Request.Form["y"])
+                    };
+
+                    ViewBag.Result = calc.Calc();
+                }
+                catch
+                {
+                    ViewBag.Result = "Invalid input";
+                }
+
+                return View("Result");
+            }
+            return View("Window");
+        }
+
+
+
+        [HttpGet]
+        [ActionName("ManualWithSeparateHandlers")]
+        public IActionResult ManualParsingSeparateGet()
+        {
+            return View("Window");
+        }
+        [HttpPost]
+        [ActionName("ManualWithSeparateHandlers")]
+        public IActionResult ManualParsingSeparatePost()
+        {
+            try
+            {
+                var calc = new CalcModel
+                {
+                    X = Int32.Parse(HttpContext.Request.Form["x"]),
+                    operation = HttpContext.Request.Form["operation"],
+                    Y = Int32.Parse(HttpContext.Request.Form["y"])
+                };
+
+                ViewBag.Result = calc.Calc();
+            }
+            catch
+            {
+                ViewBag.Result = "Invalid input";
+            }
+
+            return View("Result");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ModelBindingInParameters()
+        {
+            return View("Window");
+        }
+        [HttpPost]
+        public IActionResult ModelBindingInParameters(int x, string operation, int y)
+        {
+            if (ModelState.IsValid)
+            {
+                var calc = new CalcModel
+                {
+                    X = x,
+                    operation = operation,
+                    Y = y
+                };
+                ViewBag.Result = calc.Calc();
+            }
+            else
+            {
+                ViewBag.Result = "Invalid input";
+            }
+
+            return View("Result");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ModelBindingInSeparateModel()
+        {
+            return View("Window");
+        }
+        [HttpPost]
+        public IActionResult ModelBindingInSeparateModel(CalcModel model)
+        {
+            ViewBag.Result = ModelState.IsValid
+                ? model.Calc()
+                : "Invalid input";
+
+            return View("Result");
+        }
+    }
+}
---
+++ lab12/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=CalcService}/{action=Index}");
+
+app.Run();
